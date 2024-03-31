cd6937fcf1a63dfece977cd6254dffaac715f81c
Dawn Inator <126039858+Dawn-Inator@users.noreply.github.com>
2024-03-31 20:15:37 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -546,7 +546,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>ChatGPT</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              https://github.com/Dawn-Inator
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
